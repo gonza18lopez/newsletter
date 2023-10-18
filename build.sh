78558224d60ea0ed35b44aa886c928cfa7b29e4a
Gonza Lopez <gonzalo@digitalf2f.com>
2023-10-18 03:41:12 -0300

    fix: :bug: fix submodules script pull

--- build.sh
+++ build.sh
@@ -1,21 +1,12 @@
 #!/bin/bash
 
-# Variables
-CURRENT_DIR=$(pwd)
+# Array de submódulos
+submodules=("backend" "frontend")
 
-# Function to update submodules
-update_submodules() {
-    echo "Actualizando submódulos..."
-    git submodule update --recursive --remote
-}
+for submodule in "${submodules[@]}"; do
+  echo "Actualizando submódulo: $submodule"
+  (cd "$submodule" && git pull)
+  echo "Actualización de $submodule completada."
+done
 
-# Main script
-if [ -e .gitmodules ]; then
-    update_submodules
-    echo "Todos los submódulos se han actualizado correctamente."
-else
-    echo "Este repositorio no tiene submódulos."
-fi
-
-# Change to the root directory
-cd "$CURRENT_DIR"
+echo "Todas las actualizaciones de submódulos se han completado."
